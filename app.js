2ecfb37fdeb32c41b6b437b5677f309904939296
htariku <hgtariku@yahoo.com>
2022-01-17 17:24:30 -0500

    capture user input

---
+++ app.js
@@ -0,0 +1,19 @@
+var message = "Hello Node!";
+var sum = 5 + 3;
+
+
+console.log(message);
+console.log(sum);
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+};
+printProfileData(profileDataArgs);
